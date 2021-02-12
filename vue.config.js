182a2f929d2e23aee0344372a99058fcbfc12c4f
rice_crust <1443464276@qq.com>
2021-02-12 18:31:39 +0800

    删除

--- vue.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    devServer: {
-        host: 'localhost' // http://localhost:8080/sockjs-node/info
-        // host: '0.0.0.0', // http://192.168.*.*:8080/sockjs-node/info
-        // disableHostCheck: true, // 为 true 时，将会跳过 host 检查。这是不推荐的因为不检查host的应用容易受到DNS重新绑定攻击。
-
-
-    }
-};
