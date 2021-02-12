ef1facc07093706e21ca5be1f4f53d5022532e38
rice_crust <1443464276@qq.com>
2021-02-12 16:57:55 +0800

    github

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,13 +6,29 @@
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component'
-import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
+import { Options, Vue } from 'vue-class-component';
+import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
 @Options({
-  components: {
-    HelloWorld
-  }
+    components: {
+        HelloWorld
+    }
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+    a () {
+        let ab = 90;
+        let b = 78;
+
+
+
+        for (let i = 0; i < 100; i++) {
+            if (i === b) {
+                b = i;
+                if (ab === b + i) {
+                    ab = i * 10;
+                }
+            }
+        }
+    }
+}
 </script>
